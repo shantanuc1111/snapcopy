1905773fcc6bd3dd6440d8239518abe838ef0c9b
shantanuc1111 <51231873+shantanuc1111@users.noreply.github.com>
2022-01-06 00:11:35 +0530

    Update snapcopy.sh
    Fixed the issue where the mp4 is renamed as jpg

--- snapcopy.sh
+++ snapcopy.sh
@@ -6,6 +6,14 @@ cd "${0%/*}"
 echo Deleting Useless files
 find . -type 'f' -size -100c -delete
 echo Renaming files
-for file in *.0; do mv "$file" "${file/.0/.jpg}"; done
+for f in *.0; do 
+ext=$(file --extension -N "$f")
+if [ ${ext:(-3)} == "fif" ]
+then 
+    mv "$f" "${f/.0/.jpg}"
+else 
+    mv "$f" "${f/.0/.mp4}"
+fi
+done
 for file in *.1; do mv "$file" "${file/.1/.mp4}"; done
 for file in *.2; do mv "$file" "${file/.2/.png}"; done
